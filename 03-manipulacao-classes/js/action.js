79e7a12d7a89ea1b4ef89c1039335e6b00eb6212
Marcio Leite <marcio.leite@capitalconsig.local>
2024-01-09 12:21:45 -0300

    funções as2D as3D as4D as5D para manipulação de dimensionalidade

--- 03-manipulacao-classes/js/action.js
+++ 03-manipulacao-classes/js/action.js
@@ -12,6 +12,18 @@ function executar(){
     const tensor1 = tf.tensor([1,2,3,4] , [2,2]);
     const as1D = tensor1.as1D()
 
+    const tensor2 = tf.tensor([1,2,3,4] , [2,2,1]);
+    const as2D = tensor2.as2D(2,2)
+
+    const tensor3 = tf.tensor([1,2,3,4] , [2,2,1,1]);
+    const as3D = tensor3.as3D(2,2,1)
+
+    const tensor4 = tf.tensor([1,2,3,4]);
+    const as4D = tensor4.as4D(1,2,2,1)
+
+    const tensor5 = tf.tensor([1,2,3,4,5,6,7,8]);
+    const as5D = tensor5.as5D(1,2,2,2,1)
+
     txt += 'asScalar:\n\n';
     txt += 'Antes:\n';
     txt += tensor1d.toString() + '\n\n';
@@ -29,6 +41,30 @@ function executar(){
     txt += tensor1.toString() + '\n\n';
     txt += 'DEPOIS:\n';
     txt += as1D.toString() + '\n\n';
+
+    txt += 'as2D:\n\n';
+    txt += 'Antes:\n';
+    txt += tensor2.toString() + '\n\n';
+    txt += 'DEPOIS:\n';
+    txt += as2D.toString() + '\n\n';
+
+    txt += 'as3D:\n\n';
+    txt += 'Antes:\n';
+    txt += tensor3.toString() + '\n\n';
+    txt += 'DEPOIS:\n';
+    txt += as3D.toString() + '\n\n';
+
+    txt += 'as4D:\n\n';
+    txt += 'Antes:\n';
+    txt += tensor4.toString() + '\n\n';
+    txt += 'DEPOIS:\n';
+    txt += as4D.toString() + '\n\n';
+
+    txt += 'as5D:\n\n';
+    txt += 'Antes:\n';
+    txt += tensor5.toString() + '\n\n';
+    txt += 'DEPOIS:\n';
+    txt += as5D.toString() + '\n\n';
 
     exibir(txt)
 }
