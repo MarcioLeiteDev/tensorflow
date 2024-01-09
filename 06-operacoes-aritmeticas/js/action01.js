d0d5e3fde8cea70041e599b3b29b8bf460202438
Marcio Leite <marcio.leite@capitalconsig.local>
2024-01-09 17:17:26 -0300

    subtraindo elementos com função sub

--- 06-operacoes-aritmeticas/js/action01.js
+++ 06-operacoes-aritmeticas/js/action01.js
@@ -7,12 +7,25 @@ function executar(){
     const tensor2 = tf.tensor([10,20,30,40]);
     const add = tensor1.add(tensor2);
 
+    const tensor3 = tf.tensor([10,20,30,40]);
+    const tensor4 = tf.tensor([1,2,3,4]);
+    const sub = tensor3.sub(tensor4);
+
+
+
     txt += 'add:\n\n';
     txt += 'Antes:\n';
     txt += tensor1.toString() + '\n\n';
     txt += tensor2.toString() + '\n\n';
     txt += 'Depois:\n';
     txt += add.toString() + '\n\n';
+
+    txt += 'sub:\n\n';
+    txt += 'Antes:\n';
+    txt += tensor3.toString() + '\n\n';
+    txt += tensor4.toString() + '\n\n';
+    txt += 'Depois:\n';
+    txt += sub.toString() + '\n\n';
 
     exibir(txt)
 }
