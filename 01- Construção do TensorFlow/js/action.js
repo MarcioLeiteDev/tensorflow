bb8e6f29f8b2b05b1a72eb1b038184d0bdca5654
Marcio Leite <marcio.leite@capitalconsig.local>
2024-01-09 10:11:48 -0300

    tipagem explicita na construção do tensor

--- 01- Construção do TensorFlow/js/action.js
+++ 01- Construção do TensorFlow/js/action.js
@@ -7,21 +7,19 @@ function executar(){
 
     const tensor1d = tf.tensor1d([1,2,3]);
 
-    // const tensor2d = tf.tensor2d([1,2,3,4], [2,2]);
-    const tensor2d = tf.tensor2d([[1,2] , [3,4]]);
+    const tensor2d = tf.tensor2d([1,2,3,4], [2,2]);
+    // const tensor2d = tf.tensor2d([[1,2] , [3,4]]);
 
     // const tensor3d = tf.tensor3d([[ [1],[2]], [ [3],[4] ]]);
     const tensor3d = tf.tensor3d([1,2,3,4] , [2,2,1]);
 
-    // const tensor4d = tf.tensor4d([[[[1],[2]], [[3],[4]]]]);
+    // const tensor4d = tf.tensor4d([[[ [1],[2]], [[3],[4] ]]]);
     const tensor4d = tf.tensor4d([1,2,3,4] , [1,2,2,1]);
 
-
-    // const tensor5d = tf.tensor5d([[[[[1],[2] , [3], [4]]]]]);
+    // const tensor5d = tf.tensor5d([[[ [ [1],[2] , [3], [4] ] ]]]);
     const tensor5d = tf.tensor5d([1,2,3,4,5,6,7,8], [1,2,2,2,1]);
 
-
-    // const tensor6d = tf.tensor6d([[[[[[1],[2]], [[3],[4]]], [[[5],[6]], [[7], [8]]]]]]);
+    // const tensor6d = tf.tensor6d([[[[ [ [1],[2]], [[3],[4]]], [[[5],[6]], [[7], [8] ] ]]]]);
     const tensor6d = tf.tensor6d([1,2,3,4,5,6,7,8] , [1,1,2,2,2,1]);
 
     txt += 'Tensor Escalar: \n' + escalar.toString() + '\n\n';
