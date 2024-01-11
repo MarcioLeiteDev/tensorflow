da68e0be859107b5f4755bbd47f459d6af0684fe
Marcio Leite <marcio.leite@capitalconsig.local>
2024-01-11 12:46:33 -0300

    retornando verdadeiro para numeros maiores com greater

--- 10-operacoes-logicas/js/action01.js
+++ 10-operacoes-logicas/js/action01.js
@@ -7,13 +7,24 @@ function executar(){
     const tensor2 = tf.tensor([1,0,3,5]);
     const equal = tensor1.equal(tensor2);
 
+    const tensor3 = tf.tensor([1,2,3,7]);
+    const tensor4 = tf.tensor([1,0,3,5]);
+    const greater = tensor3.greater(tensor4);
 
-    txt += 'sum:\n\n';
-    txt += 'Antens:\n';
+
+    txt += 'equal:\n\n';
+    txt += 'Antes:\n';
     txt += tensor1.toString() + '\n\n';
     txt += tensor2.toString() + '\n\n';
     txt += 'Depois:\n';
     txt += equal.toString() + '\n\n';
+
+    txt += 'greater:\n\n';
+    txt += 'Antes:\n';
+    txt += tensor3.toString() + '\n\n';
+    txt += tensor4.toString() + '\n\n';
+    txt += 'Depois:\n';
+    txt += greater.toString() + '\n\n';
 
 
     exibir(txt)
